c0e972dcf6141dc4bb8e451be74474271059c54e
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2025-05-11 10:08:52 +0530

    Shows how to use this keyword

--- day13-may-11/this.js
+++ day13-may-11/this.js
@@ -4,6 +4,17 @@ const goat = {
   makesound() {
     console.log("baa");
   },
+  diet() {
+    // console.log(goat.dietType);
+
+    // console.log(goat[dietType]);
+    /* dietType is not defined error */ /* cause it needs "" double quotation */
+
+    // console.log(goat["dietType"]);
+
+    // console.log(this["dietType"]);
+    console.log(this.dietType);
+  },
 };
 
 goat.makesound();
@@ -13,4 +24,21 @@ const addMe = (obj) => {
 };
 
 addMe(goat);
-console.log(goat);
+goat.diet(); /* Reference error diet type is not defined */
+// it is strange , why is dietType not defined even though it's a property of goat object , because inside the scope of the .diet() method , we don't automatically have access to other properties of the goat object.
+
+/* Here this keyword comes into play , if we change .diet() method to use this, which will look like  this.dietType */
+
+/*
+ const goat = {
+dietType : 'herbivore',
+makesound(){
+  console.log("baa");
+},
+  diet(){
+console.log(this.dietType);
+ }
+  }; 
+*/
+
+// The this keyword references the calling object which provides access to the calling ojbect's properties.
